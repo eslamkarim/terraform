b2b964a2817dc61261ebc0ca7a17fd47c67b2081
eslamkarim <eslamkarim222@gmail.com>
2020-04-05 15:49:12 +0200

    fix errors

--- ssh.tf
+++ ssh.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "terraform" {
 
 resource "aws_key_pair" "generated_key" {
   key_name   = "ec2-key"
-  public_key = "${tls_private_key.example.public_key_openssh}"
+  public_key = "${tls_private_key.terraform.public_key_openssh}"
 }
 
 resource "aws_secretsmanager_secret" "private-key-secret" {
@@ -14,5 +14,5 @@ resource "aws_secretsmanager_secret" "private-key-secret" {
 
 resource "aws_secretsmanager_secret_version" "terraform" {
   secret_id     = "${aws_secretsmanager_secret.private-key-secret.id}"
-  secret_string = "${tls_private_key.example.private_key_pem}"
+  secret_string = "${tls_private_key.terraform.private_key_pem}"
 }
